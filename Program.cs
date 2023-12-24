466586d3eb6207d9d0f7223bd9077f6f3ea5a5ae
anascode23 <anas.ali@ecutbildning.se>
2023-12-24 15:28:42 +0000

    added logic

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿List<string> list = [];
+
+
+while (true)
+{
+    Console.WriteLine("Please enter what you want to do with you list:  \nadd, \nremove, \nshow list, \nexit:  ");
+    var choice = Console.ReadLine();
+    if (choice == "exit" || choice == "quit")
+        break;
+
+    switch (choice)
+    {
+        case "add":
+            if (choice == "add")
+            {
+                Console.WriteLine("Please enter the items spearted by comma eg.(apple, banana)");
+                var addingItem = Console.ReadLine();
+
+                Console.WriteLine("----------------------------------------------------------------");
+                if (!string.IsNullOrWhiteSpace(addingItem)){
+                    list.Add(addingItem);
+                    Console.WriteLine("Item(s) added");
+                }
+                    
+
+                else
+                    Console.WriteLine("Invaild item please try again");
+            }
+            break;
+
+
+        case "remove":
+            if (choice == "remove")
+            {
+                Console.WriteLine("Please enter the item you want to remove");
+                var itemToRemove = Console.ReadLine();
+                Console.WriteLine("----------------------------------------------------------------");
+                for (int i = list.Count - 1; i >= 0; i--)
+                {
+                    if (list[i] == itemToRemove)
+                    {
+                        list.RemoveAt(i);
+                        Console.WriteLine("Item removed!");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Item not found please try again");
+                }
+            }
+
+            break;
+
+        case "show list":
+            if (choice == "show list")
+            {
+                Console.WriteLine("----------------------------------------------------------------");
+                foreach (var item in list)
+                    Console.WriteLine("Grocery list: " + item);
+
+                Console.WriteLine("----------------------------------------------------------------");
+            }
+
+            break;
+
+        default:
+            Console.WriteLine("Invaild choice please try again");
+            continue;
+    }
+}
